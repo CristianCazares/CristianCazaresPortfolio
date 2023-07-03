f6b57461c25d66c2bbaa2df416e362cad0478de8
CristianCazares <cm_cjavier@hotmail.com>
2023-07-03 11:46:46 -0600

    feat(layout): added basic project cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import { Navbar } from "./components";
 import styles from "./page.module.scss";
 import CristianPhoto from "@/../public/img/CristianCazares.jpg";
@@ -10,7 +11,9 @@ const Home = () => {
       <main className={styles.main}>
         <div className={styles.container}>
           <div className={styles.header}>
-            <div className={`${styles.cardContainer} ${styles.imageContainer}`}>
+            <div
+              className={`${styles.cardContainerHeader} ${styles.imageContainer}`}
+            >
               <Image
                 className={styles.image}
                 alt="Cristian's photo"
@@ -18,17 +21,30 @@ const Home = () => {
                 height={400}
               />
             </div>
-            <div className={styles.cardContainer} />
+            <div className={styles.cardContainerHeader} />
           </div>
           <div className={styles.content}>
-            <div className={styles.cardContainer} />
-            <div className={styles.cardContainer} />
-            <div className={styles.cardContainer} />
-            <div className={styles.cardContainer} />
+            <Link href="https://github.com/Bugs-io/ibm-dashboard-app">
+              <div className={styles.cardContainer}>
+                IBM Strategic dashboard
+              </div>
+            </Link>
+            <Link href="https://github.com/FoodBank-GDL/FoodBank-ReactNative">
+              <div className={styles.cardContainer}>Food Bank</div>
+            </Link>
+            <Link href="https://github.com/ITESMECHO/MultiagentsGraphics">
+              <div className={styles.cardContainer}>Multiagents</div>
+            </Link>
+            <Link href="https://github.com/HackMTY-Team-6/HackMTY-Team6">
+              <div className={styles.cardContainer}>Blood Manager</div>
+            </Link>
+            <Link href="https://github.com/CarlosCRG19/ijalti-peers">
+              <div className={styles.cardContainer}>Ijalti Peers</div>
+            </Link>
           </div>
         </div>
       </main>
-      <footer style={{height: 24}}></footer>
+      <footer style={{ height: 24 }}></footer>
     </>
   );
 };
--- src/utils/types/index.ts
+++ src/utils/types/index.ts
@@ -2,3 +2,10 @@ export type BulletPoint = {
   emoticon: string;
   text: string;
 };
+
+export type ProjectBasicInfo = {
+  title: string;
+  subtitle: string;
+  url: string;
+  image: string;
+};
