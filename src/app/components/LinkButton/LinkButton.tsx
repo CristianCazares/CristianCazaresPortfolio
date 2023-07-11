69a8a002c76c9a688566b850759737a74ff9fd0d
CristianCazares <cm_cjavier@hotmail.com>
2023-07-11 14:54:37 -0600

    feat(LinkButton): added a basic button for links

---
+++ src/app/components/LinkButton/LinkButton.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import styles from "./linkButton.module.scss";
+
+interface Props {
+  url: string;
+  children: React.ReactNode;
+  icon?: React.ReactElement<any, any>;
+  targetBlank?: boolean;
+  size?: "md" | "sm" | "lg" | "xl";
+}
+
+const LinkButton = ({
+  url,
+  children,
+  icon,
+  targetBlank = false,
+  size = "sm",
+}: Props) => {
+  return (
+    <div className={styles.container}>
+      <Link
+        href={url}
+        target={targetBlank ? "_blank" : ""}
+        className={`${styles.link} ${styles[size]}`}
+      >
+        {icon}
+        {children}
+      </Link>
+    </div>
+  );
+};
+
+export default LinkButton;
