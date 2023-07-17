b9c2d9abaa7b171bff64411d9eac5152abc1dcd8
CristianCazares <cm_cjavier@hotmail.com>
2023-07-16 18:38:18 -0600

    feat(project/loading): add styled and animated page loader

---
+++ src/app/components/PageLoaders/PageLoader.tsx
@@ -0,0 +1,14 @@
+import styles from "./pageLoader.module.scss";
+
+const PageLoader = () => {
+  return (
+    <div className={styles["lds-ring"]}>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default PageLoader;
--- src/app/project/[id]/loading.tsx
+++ src/app/project/[id]/loading.tsx
@@ -1,5 +1,18 @@
+import PageLoader from "@/app/components/PageLoaders";
+
 const Loading = () => {
-  return <div>Loading...</div>;
+  return (
+    <div
+      style={{
+        height: "50vh",
+        width: "100%",
+        display: "grid",
+        placeItems: "center",
+      }}
+    >
+      <PageLoader />
+    </div>
+  );
 };
 
 export default Loading;
