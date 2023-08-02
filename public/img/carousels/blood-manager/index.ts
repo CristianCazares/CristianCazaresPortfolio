5e9991620a3d65c50b036ed9dd3ba4f90e481cbf
CristianCazares <cm_cjavier@hotmail.com>
2023-08-01 20:59:43 -0600

    feat(project/blood-manager): enhanced project's showcase images

--- public/img/carousels/blood-manager/index.ts
+++ public/img/carousels/blood-manager/index.ts
@@ -1,22 +1,25 @@
 import CarouselItem from "@/lib/CarouselItem";
 import auth from "./auth.png";
-import askForm from "./askForm.png";
-import feedDonator from "./feedDonator.png";
-import feedReceiver from "./feedReceiver.png";
-import form from "./form.png";
-import login from "./login.png";
-import main from "./main.png";
-import signup from "./signup.png";
+import donorForm from "./donorForm.png";
+import pickAccount from "./pickAccount.png";
+import solicitantAndDonor from "./solicitantAndDonor.png";
+import solicitantFeed from "./solicitantFeed.png";
 
 const bloodManagerCarousel: CarouselItem[] = [
-  { image: main, legend: "A proposal to securely manage blood donations" },
-  { image: auth, legend: "Strong authentication" },
-  { image: form, legend: "Based on real life medical standards" },
-  { image: askForm, legend: "Ask for donations" },
-  { image: feedDonator, legend: "Look at people asking for donations" },
   {
-    image: feedReceiver,
-    legend: "Only essential information about your donors",
+    image: pickAccount,
+    legend: "A proposal to securely manage blood donations",
+  },
+  { image: auth, legend: "Strong authentication" },
+  { image: donorForm, legend: "Based on real life medical standards" },
+  {
+    image: solicitantAndDonor,
+    legend: "Interaction between diferent types of users",
+  },
+  {
+    image: solicitantFeed,
+    legend:
+      "Prioritizing privacy by only sharing essential information about donors",
   },
 ];
 
